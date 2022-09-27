c31df007c77cb8cc66cd57d49c299bc3c0696867
rahmatsyalim <rahmatsyalim@gmail.com>
2022-09-28 01:56:52 +0700

    Updated theme configurations

--- app/src/main/java/com/syalim/edufundtest/ui/theme/Color.kt
+++ app/src/main/java/com/syalim/edufundtest/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.syalim.edufundtest.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val themePrimary = Color(0xFFEC407A)
+val themePrimaryVariant = Color(0xFFB6335F)
+val themeSecondary = Color(0xFF00AFFF)
+val themeSecondaryVariant = Color(0xFF007AB1)
---
+++ app/src/main/java/com/syalim/edufundtest/ui/theme/Dimen.kt
@@ -0,0 +1,15 @@
+package com.syalim.edufundtest.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+
+/**
+ * Created by Rahmat Syalim on 2022/09/28
+ * rahmatsyalim@gmail.com
+ */
+
+val dimenTiny = 3.dp
+val dimenSmall = 6.dp
+val dimenRegular = 12.dp
+val dimenMedium = 18.dp
+val dimenLarge = 24.dp
